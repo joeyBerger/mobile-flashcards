a8b7b50f731204abff36cfdfba7521bbe06c9a39
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2020-03-16 20:21:06 -0700

    added provider/store

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
 import Constants from 'expo-constants';
 import DeckList from './components/DeckList'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
+import reducer from './reducers'
 
 function Feed() {
   return (
@@ -87,19 +90,21 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
   },
 });
-//createMaterialTopTabNavigator
-export default function App() {
-  return (
-    <View style = {styles.container}> 
-      <NavigationContainer >
 
-        <View style={{ backgroundColor: 'purple', height:Constants.statusBarHeight}}>
-          <StatusBar translucent/>
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style = {styles.container}> 
+          <NavigationContainer >
+            <View style={{ backgroundColor: 'purple', height:Constants.statusBarHeight}}>
+              <StatusBar translucent/>
+            </View>
+            <MyTabs />
+          </NavigationContainer>
         </View>
-
-        <MyTabs />
-      </NavigationContainer>
-    </View>
-  );
+      </Provider>
+    )
+  }
 }
 
---
+++ actions/index.js
@@ -0,0 +1,40 @@
+export const RECEIVE_DECKS = 'RECEIVE_DECKS'
+export const ADD_DECK = 'ADD_DECK'
+export const REMOVE_DECK = 'REMOVE_DECK'
+export const ADD_DECK_QUESTION = 'ADD_DECK_QUESTION'
+export const REMOVE_DECK_QUESTION = 'REMOVE_DECK_QUESTION'
+
+export function recieveDecks(decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks
+  }
+}
+
+export function addDeck(deck) {
+  return {
+    type: ADD_DECK,
+    deck
+  }
+}
+
+export function removeDeck(deck) {
+  return {
+    type: REMOVE_DECK,
+    deck
+  }
+}
+
+export function addDeckQuestion(question) {
+  return {
+    type: ADD_DECK_QUESTION,
+    question
+  }
+}
+
+export function removeDeckQuestion(question) {
+  return {
+    type: REMOVE_DECK_QUESTION,
+    question
+  }
+}
